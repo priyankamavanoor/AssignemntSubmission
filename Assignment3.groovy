f9b653c3e23d38c276da010714253f8202467350
priyankamavanoor <105637119+priyankamavanoor@users.noreply.github.com>
2022-07-02 02:39:55 -0700

    Update Assignment3.groovy

--- Assignment3.groovy
+++ Assignment3.groovy
@@ -10,5 +10,22 @@ Output: Hari => 2, Francis => 1, Naveen => 2, Harish => 1, Thejus => 2, Pritika 
 def list1 = ["Hari","Francis","Naveen"]
 def list2 = ["Harish","Thejus","Naveen", "Partha"]
 def list3 = ["Hari","Thejus","Pritika","Subha"]
-
-println ([list1, list2, list3].transpose().collect{it -> it[0] + it[1]})
+def list = []
+def counterMap =[:]
+list1.eachWithIndex { value,index ->
+	list.add(value)
+}
+list2.eachWithIndex { value,index ->
+	list.add(value)
+}
+list3.eachWithIndex { value,index ->
+	list.add(value)
+}
+println list
+for(item in list) {
+	if(counterMap.get(item)!=null) {
+		counterMap.put(item, counterMap.get(item)+1)
+	}else {
+		counterMap.put(item,1)
+	}
+}
